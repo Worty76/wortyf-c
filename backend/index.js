370cde0f7ed1fb5289e1f53b82ce1c9bc7341739
Worty76 <lethanhdat762003@gmail.com>
2024-10-08 12:41:25 +0700

    fix(socket): set polling as a fallback

--- backend/index.js
+++ backend/index.js
@@ -87,7 +87,7 @@ const io = require("socket.io")(server, {
     origin: process.env.API,
     credentials: true,
     methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
+    transports: ["polling", "websocket"],
   },
   allowEIO3: true,
 });
